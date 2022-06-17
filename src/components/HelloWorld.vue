9bafd6f4e59ff9981ec2df100162646a3f73064d
황태희 <tevem1207@gmail.com>
2022-06-17 19:43:37 +0900

    0617

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h1>이게 바로 Vue야</h1>
+    <p>그렇군요~</p>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
